d0477fc9da4130e3f5a770c496070147e7de858e
Tu Nombre <tu-email@example.com>
2026-02-19 08:39:46 +0100

    Remove basePath for subdomain config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'standalone',
     /* config options here */
-    basePath: '/tienda',
 };
 
 export default nextConfig;
